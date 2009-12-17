859174b9b88c2fcc3e54acd94d01f27f501be8bb
mazgalici <mazgalici@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-17 00:01:21 +0000

    added categories exclusion
    git-svn-id: https://plugins.svn.wordpress.org/linktoecard/trunk@183628 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- _config.php
+++ _config.php
@@ -3,7 +3,7 @@
 $linkToEcardDefaultText='<center><table><tr><td valign="middle"><img src="http://messengerinvisible.com/ecard.gif" border="0"></td><td valign="middle">Send this picture as an ecard</td></table></center>';
 $linkToEcardTextEmail='Check this site [url]!';
 $linkToEcardLang='en';
-	$linkToEcardCategories=0;
+	$linkToEcardCategories='0,';
 
 $linkToEcardLanguages=array(
 'English'=>'en',
