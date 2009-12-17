4a06ad22c38ba0ea85da477cd3427cdd7cac717f
mazgalici <mazgalici@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-17 20:42:21 +0000

    added tags and categories excluding
    git-svn-id: https://plugins.svn.wordpress.org/linktoecard/trunk@184060 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- linkToEcard.php
+++ linkToEcard.php
@@ -1,9 +1,9 @@
 <?php
 /*
-Plugin Name: LinkToEcard
+Plugin Name: Link to ecard
 Plugin URI: http://LinkToEcard.com/wordpress-plugin
-Description: Every image from you blog can me sent as an ecard
-Version: 1.2.2
+Description: Every image from you blog can be sent as an ecard
+Version: 1.3.0
 Author: Mazgalici
 */
 
@@ -24,19 +24,32 @@ function linkToEcardAdmin(){
 
 
 	if (isset($_POST['sent'])){
+		//print_r($_POST['onlyOnInteriorPages']);
+
 		update_option('linkToEcardTextLink',trim($_POST['textLink']));
 		update_option('linkToEcardTextEmail',trim($_POST['textEmail']));
 		update_option('linkToEcardLang',trim($_POST['lang']));
 		update_option('linkToEcardCategories',@implode(',',$_POST['categs']));
+		update_option('linkToEcardTags',strtolower(trim(str_replace('  ',' ',$_POST['tags']))));
+		update_option('linkToEcardPosts',strtolower(trim(str_replace('  ',' ',$_POST['posts']))));
+
+		if ($_POST['onlyOnInteriorPages']=='on'){
+			$_POST['onlyOnInteriorPages']=1;
+		}
+		else {
+			$_POST['onlyOnInteriorPages']=0;
+		}
+
+		
+		update_option('linkToEcardOnlyOnInteriorPages',$_POST['onlyOnInteriorPages']);
 
 	}
 
 
 
-	$out='<div class="wrap">
-	<h2>Link to ecard settings</h2>
-</div>';
-	$out.='<br><br><form action="" method="POST">';
+	$out='<div class="wrap"><h2>Link to ecard settings</h2></div>';
+	
+	$out.='<br><form action="" method="POST">';
 	$out.='<table>';
 	$out.='<tr><td>HTML on your blog</td>';
 	$out.='<td><textarea name="textLink" style="width:450px">'.stripslashes(get_option('linkToEcardTextLink')).'</textarea></td></tr>';
@@ -47,22 +60,24 @@ function linkToEcardAdmin(){
 
 	$categories = get_categories(array('hide_empty'=>'false'));
 
-	$out.='<tr><td>Enable plugin for <br>
+	$out.='<tr><td>Enable plugin for this categories<br>
 		<i><small>CTRL+Click for multiple selection</small></i>
 	</td>';
-	$out.='<td><select name="categs[]" size="10" style="height:100px" multiple="multiple">';
+	$out.='<td valign="middle"><select name="categs[]" size="10" style="height:100px" multiple="multiple">';
 	$selectedCategs=explode(',',get_option('linkToEcardCategories'));
-	
+
 	$selected='';
-		if (in_array('0',$selectedCategs)){
-			$selected='selected';
-		}
+	if (in_array('0',$selectedCategs)){
+		$selected='selected';
+	}
 	$out.='<option '.$selected.' value="0">All categs</option>';
 
-	
+
 
 	for ($i=0;$i<sizeof($categories);$i++){
-		if (strlen($categories[$i]->cat_ID)<1) continue;
+		if (strlen($categories[$i]->cat_ID)<1){
+			continue;
+		}
 		$selected='';
 
 		if (in_array($categories[$i]->cat_ID,$selectedCategs)){
@@ -71,8 +86,23 @@ function linkToEcardAdmin(){
 
 		$out.='<option '.$selected.' value="'.$categories[$i]->cat_ID.'">'.$categories[$i]->cat_name.'</option>';
 	}
-	$out.='</select></td></tr>';
+	$out.='</select> <i><small>Categories with 0 posts aren\'t displayed here</small></i></td></tr>';
 	//print_r($categories);
+
+	$out.='<tr><td> Enable plugin for this tags <br>
+	<i><small>Ex: tag1,tag2,tag3</small></i>
+	</td><td><input type="text" name="tags" value="'.get_option('linkToEcardTags').'" style="width:450px" ></td></tr>';
+
+	$out.='<tr><td> Enable plugin for this posts ids<br>
+	<i><small>Ex: 11,23,45</small></i>
+	</td><td><input type="text" name="posts" value="'.get_option('linkToEcardPosts').'" style="width:450px" ></td></tr>';
+
+/*
+	$out.='<tr><td>Enable only for interior pages</td><td><input type="checkbox" name="onlyOnInteriorPages" ';
+	if (get_option('linkToEcardOnlyOnInteriorPages')==1){
+		$out.=' checked ';
+	}
+	$out.='> </td></tr>';*/
 
 	$out.='<tr><td>Ecards form language <br>
 		<i><small>More languages on request<small></i>
@@ -90,11 +120,13 @@ function linkToEcardAdmin(){
 
 	$out.=$outLang;
 	$out.='</select></td></tr>';
+
+
 	$out.="<tr><td  colspan='2' align='right'><input type='submit' value='  Update  '></td>";
 	$out.='</table>';
 	$out.='<input type="hidden" name="sent" value="1"> </form>';
-			
-	$out.='<br><br><br><small><i>Contact me: <a href="mailto:fcmazgalici@yahoo.com">fcmazgalici@yahoo.com</a></small></i>';	
+
+	$out.='<br><small><i>Contact for bugs and curses <a href="mailto:fcmazgalici@yahoo.com">fcmazgalici@yahoo.com</a></small></i>';
 	echo $out;
 
 }
@@ -106,21 +138,33 @@ function linkToEcardInstall(){
 	$linkToEcardTextEmail='Check this site [url]!';
 	$linkToEcardLang='en';
 	$linkToEcardCategories=0;
+	$linkToEcardTags='';
+	$linkToEcardPosts='';
+	$linkToEcardOnlyOnInteriorPages='0';
 
 	add_option('linkToEcardTextLink',$linkToEcardDefaultText);
 	add_option('linkToEcardTextEmail',$linkToEcardTextEmail);
 	add_option('linkToEcardLang',$linkToEcardLang);
 	add_option('linkToEcardCategories',$linkToEcardCategories);
+	add_option('linkToEcardTags',$linkToEcardTags);
+	add_option('linkToEcardPosts',$linkToEcardPosts);
+	add_option('linkToEcardOnlyOnInteriorPages',$linkToEcardOnlyOnInteriorPages);
 
 }
 
 function linkToEcardTheContent($content){
-
 	global $post;
 
+	//echo "x".is_single($post->ID);
+	/*if (get_option('linkToEcardOnlyOnInteriorPages')==1 && is_single($post->ID)){
+		return $content;
+	}*/
+	
 	$categories=get_the_category();
+	$tags=get_the_tags();
 	$enabledCategories=explode(',',get_option('linkToEcardCategories'));
-	//print_r($enabledCategories);
+
+
 
 	if (in_array('0',$enabledCategories)){
 		$enabled=true;
@@ -137,6 +181,32 @@ function linkToEcardTheContent($content){
 			}
 		}
 	}
+
+	if (!$enabled && sizeof($tags)>1){
+		$linkToEcardTags=explode(',',get_option('linkToEcardTags'));
+		if (sizeof($linkToEcardTags)>0){
+			for ($i=0;$i<sizeof($linkToEcardTags);$i++){
+				foreach ($tags as $tag){
+					if ($linkToEcardTags[$i]==strtolower(trim($tag->name))){
+						$enabled=true;
+						break;
+					}
+				}
+
+			}
+		}
+	}
+
+	if (!$enabled){
+		$linkToEcardPosts=explode(',',get_option('linkToEcardPosts'));
+		for ($i=0;$i<sizeof($linkToEcardPosts);$i++){
+			if ($linkToEcardPosts[$i]==$post->ID){
+				$enabled=true;
+				break;
+			}
+		}
+	}
+
 
 	if ($enabled){
 		$content=preg_replace('|(<img.+?src="([^ ]+)".+?>)|','$0</a><br><a target="_blank" href="http://linkToEcard.com/?u=$2&l='.get_option('linkToEcardLang').'&t='.urlencode(get_option('linkToEcardTextEmail')).'" style="text-decoration:none;border:none">'.stripslashes(get_option('linkToEcardTextLink')).'</a>',$content);
